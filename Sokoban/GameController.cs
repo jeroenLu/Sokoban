3fac4012809b7f5d46ee27aae6a043d0c4e18d23
jeroen luers <jeroen luers>
2016-10-04 23:06:03 +0200

    Get levelinout and path refactored

--- Sokoban/GameController.cs
+++ Sokoban/GameController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,9 @@ namespace Sokoban
 {
     class GameController
     {
-        Game Game;
-        BaseField[,] LoadedBoard;
+        private Game Game;
+        private BaseField[,] LoadedBoard;
+        private int _levelWidth, _levelHeight;
 
 
         internal void SetUpGame()
@@ -18,64 +21,84 @@ namespace Sokoban
             Game.StartGame();
             LoadBoard();
             Console.ReadKey();
-            for (int i = 0; i < 6; i++)
+            for (int i = 0; i < _levelHeight; i++)
             {
-                for(int x = 0; x < 9; x++)
+                for (int x = 0; x < _levelWidth; x++)
                 {
                     Console.Write(LoadedBoard[x, i]);
                 }
                 Console.WriteLine();
             }
             Console.ReadKey();
-            
+
         }
 
         private void LoadBoard()
         {
+            var chosen = false;
+            string path = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), @"levels\doolhof");
 
-            System.IO.StreamReader file = new System.IO.StreamReader("C:\\Users\\Jessew\\Documents\\Informatica\\Hoofdfase1\\Modelleren 3\\Sokoban\\Sokoban\\levels\\doolhof1.txt");
-            int height = System.IO.File.ReadAllLines(@"C:\\Users\\Jessew\\Documents\\Informatica\\Hoofdfase1\\Modelleren 3\\Sokoban\\Sokoban\\levels\\doolhof1.txt").Count();
-            int width = file.ReadLine().Length;
-            string line;
-            
-            Console.WriteLine("This is height " + height);
-            Console.WriteLine("This is width " + width);
-
-            LoadedBoard = new BaseField[width, height];
-            file = new System.IO.StreamReader("C:\\Users\\Jessew\\Documents\\Informatica\\Hoofdfase1\\Modelleren 3\\Sokoban\\Sokoban\\levels\\doolhof1.txt");
-            height = 0;
-            while((line = file.ReadLine()) != null)
+            while (!chosen)
             {
+                Console.WriteLine("Please input the level you want to select");
 
-                for (int x = 0; x < width; x++)
+                // Hier wordt input van de user gevraagd welk level hij wil laden
+                char selectedLevel;
+                if (char.TryParse(Console.ReadLine(), out selectedLevel))
+                {
+                    path += selectedLevel + ".txt";
+                    chosen = true;
+                }
+                else
+                {
+                    // Kan later evt weggehaald worden -------------------------------------------------------
+                    Console.WriteLine("Not a valid input! ");
+                }
+            }
+
+
+
+            // Hier moeten nog exceptions worden afgevangen!!!! -------------------------------------------------------------
+
+            string[] fileLines = File.ReadAllLines(path);
+
+            _levelHeight = fileLines.Length;
+            _levelWidth = fileLines[0].Length;
+
+            Console.WriteLine("This is height " + _levelHeight);
+            Console.WriteLine("This is width " + _levelWidth);
+
+            LoadedBoard = new BaseField[_levelWidth, _levelHeight];
+
+            _levelHeight = 0;
+            foreach (var line in fileLines)
+            {
+                for (int x = 0; x < _levelWidth; x++)
                 {
                     switch (line.ElementAt(x))
                     {
                         case '#':
-                            LoadedBoard[x, height] = new Wall();
+                            LoadedBoard[x, _levelHeight] = new Wall();
                             break;
                         case 'o':
-                            LoadedBoard[x, height] = new Field(new Box());
+                            LoadedBoard[x, _levelHeight] = new Field(new Box());
                             break;
                         case '.':
-                            LoadedBoard[x, height] = new Field();
+                            LoadedBoard[x, _levelHeight] = new Field();
                             break;
                         case 'x':
-                            LoadedBoard[x, height] = new EndField();
+                            LoadedBoard[x, _levelHeight] = new EndField();
                             break;
                         case '@':
-                            LoadedBoard[x, height] = new Field(new Player());
+                            LoadedBoard[x, _levelHeight] = new Field(new Player());
                             break;
                         default:
-                            LoadedBoard[x, height] = new BaseField();
+                            LoadedBoard[x, _levelHeight] = new BaseField();
                             break;
-
                     }
                 }
-                height++;
+                _levelHeight++;
             }
-                
-            
         }
     }
 }
